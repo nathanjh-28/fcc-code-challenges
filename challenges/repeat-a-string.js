729365dd5adec7fcf3da556a96a76b0115fd9c96
nathanjh-28 <nathan@nathanjharris.com>
2022-07-03 23:58:57 -0700

    recursive solution for repeat-a-string

--- challenges/repeat-a-string.js
+++ challenges/repeat-a-string.js
@@ -22,6 +22,11 @@ function repeatStringNumTimes(str, num) {
     return newString;
 }
 
+// recursive solution
+function repeatStringNumTimes(str, num) {
+    return num > 0 ? str + repeatStringNumTimes(str, num - 1) : '';
+}
+
 repeatStringNumTimes("abc", 3);
 
 
