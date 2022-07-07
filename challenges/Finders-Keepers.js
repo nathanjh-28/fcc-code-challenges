e9e1c04d0d7fe189f76caec45cdb11ce963a41f6
nathanjh-28 <nathan@nathanjharris.com>
2022-07-06 22:38:48 -0500

    finders keepers complete pt2

--- challenges/Finders-Keepers.js
+++ challenges/Finders-Keepers.js
@@ -7,10 +7,16 @@ Create a function that looks through an array arr and returns the first element 
 
 function findElement(arr, func) {
 
+    let result = undefined;
     for (let i = 0; i < arr.length; i++) {
-        if (func(arr[i])) return arr[i]
+        if (func(arr[i])) {
+            result = arr[i];
+            break;
+        }
     }
-    return undefined;
+
+    return result;
+
 }
 
-findElement([1, 2, 3, 4], num => num % 2 === 0);
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
